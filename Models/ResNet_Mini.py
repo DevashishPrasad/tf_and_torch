137289a8a1d8bc8e5ca66c0d9ed60a32bbe4ba46
Devashish Prasad <devashishkprasad@gmail.com>
2020-04-30 22:24:27 +0530

    Create ResNet_Mini.py

---
+++ Models/ResNet_Mini.py
@@ -0,0 +1,50 @@
+class Res_Type(nn.Module):
+  def __init__(self):
+    super(Res_Type, self).__init__()
+    self.layer1 = nn.Sequential(
+      nn.Conv2d(3, 32, kernel_size=7, stride=2, padding=3),
+      nn.BatchNorm2d(32),
+      nn.ReLU(),
+      nn.Conv2d(32, 32, kernel_size=5, stride=1, padding=2),
+      nn.BatchNorm2d(32),
+      nn.ReLU(),      
+      nn.MaxPool2d(kernel_size=3, stride=2))
+    self.layer2 = nn.Sequential(
+      nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+      nn.BatchNorm2d(32),
+      nn.ReLU(),
+      nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+      nn.BatchNorm2d(32),
+      nn.ReLU(),      
+      nn.MaxPool2d(kernel_size=2, stride=2))
+    self.layer3 = nn.Sequential(
+      nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+      nn.BatchNorm2d(128),
+      nn.ReLU(),
+      nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+      nn.BatchNorm2d(128),
+      nn.ReLU(),      
+      nn.MaxPool2d(kernel_size=2, stride=2))
+    self.layer4 = nn.Sequential(
+      nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+      nn.BatchNorm2d(256),
+      nn.ReLU(),
+      nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+      nn.BatchNorm2d(256),
+      nn.ReLU(),      
+      nn.AvgPool2d(12))  
+    self.fc1 = nn.Linear(128, 512)
+    self.fc2 = nn.Linear(512, 256)
+    self.fc3 = nn.Linear(256, 11)
+    
+  def forward(self, x):
+    x=x.float()
+    out = self.layer1(x)
+    out = self.layer2(out)
+    out = self.layer3(out)
+    out = self.layer4(out)
+    out = out.reshape(out.size(0), -1)
+    out = self.fc1(out)
+    out = self.fc2(out)
+    out = self.fc3(out)
+    return out
